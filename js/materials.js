562170c97c5293425cff86b04012d2dd426fe239
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2021-04-06 17:05:48 +0200

    Clean up code and fix teapot model

---
+++ js/materials.js
@@ -0,0 +1,15 @@
+
+
+const ceramicMaterial = new THREE.MeshLambertMaterial( {
+    color: 0xffffff,
+    // envMap: reflectionCube,
+    // combine: THREE.MixOperation,
+    reflectivity: 0.3
+  } );
+
+
+const basicMaterial = new THREE.MeshStandardMaterial( {
+    color: 0xCCC3B6,
+    // envMap: reflectionCube,
+    // combine: THREE.MixOperation,
+  } );
