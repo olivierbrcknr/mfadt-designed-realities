832c7a0a0e397a81d29b7c9abc7fd0710e43e87d
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2021-04-12 20:02:00 +0200

    Add wall object

--- src/script.js
+++ src/script.js
@@ -19,7 +19,8 @@ if( !isDev ){
 }
 
 const objectScales = {
-  teapot: 0.02
+  teapot: 0.02,
+  wall: 20
 }
 
 // Main Sizes
@@ -35,12 +36,28 @@ const objLoader = new OBJLoader()
 // Textures
 const matCap_porcelain = textureLoader.load('/textures/matcaps/porcelain.jpg')
 
+const matCap_vinyl_beige = textureLoader.load('/textures/matcaps/VinylBeige.jpg')
+
+const matCap_vinyl_brown = textureLoader.load('/textures/matcaps/VinylBrown.jpg')
+
+const matCap_vinyl_red = textureLoader.load('/textures/matcaps/VinylRed.jpg')
 
 // Materials
 
 const material_vinylWhite = new THREE.MeshMatcapMaterial()
 material_vinylWhite.matcap = matCap_porcelain
 material_vinylWhite.side = THREE.DoubleSide
+
+const material_vinylBeige = new THREE.MeshMatcapMaterial()
+material_vinylBeige.matcap = matCap_vinyl_beige
+
+const material_vinylBrown = new THREE.MeshMatcapMaterial()
+material_vinylBrown.matcap = matCap_vinyl_brown
+material_vinylBrown.side = THREE.DoubleSide
+
+const material_vinylRed = new THREE.MeshMatcapMaterial()
+material_vinylRed.matcap = matCap_vinyl_red
+material_vinylRed.side = THREE.DoubleSide
 
 
 // Object one — Teapot —————————————————————————————————————————————
@@ -101,7 +118,92 @@ objLoader.load(
 );
 
 
+// Object two — Wall —————————————————————————————————————————————
 
+const canvas_wall = document.querySelector('canvas.object-wall')
+
+const wall_cursor = {
+  x: 0,
+  y: 0
+}
+canvas_wall.addEventListener( "mousemove", (event) => {
+  wall_cursor.x = event.clientX / sizes.width - 0.5
+  wall_cursor.y = - ( event.clientY / sizes.height - 0.5 )
+} )
+
+
+let guiWallFolder = gui.addFolder('Wall')
+
+guiWallFolder.add(objectScales, 'wall', 10, 100, 0.01).onChange(()=>{
+  object_wall.scale.set(objectScales.wall,objectScales.wall,objectScales.wall)
+})
+
+const wall_sizes = {
+  width: sizes.width,
+  height: 600,
+  cameraY: 17,
+  cameraMovement: 10
+}
+
+
+const scene_wall = new THREE.Scene()
+
+const camera_wall = new THREE.PerspectiveCamera(22, wall_sizes.width / wall_sizes.height, 10, 200)
+camera_wall.position.y = wall_sizes.cameraY;
+camera_wall.position.z = 100;
+scene_wall.add(camera_wall)
+
+camera_wall.lookAt( new THREE.Vector3(0,17,0) )
+
+console.log(camera_wall)
+guiWallFolder.add(camera_wall.position, 'y', 10, 100, 0.01).name("Cam Y")
+guiWallFolder.add(camera_wall, 'fov', 10, 100, 0.01).name("FOV").onChange(()=>{
+  camera_wall.updateProjectionMatrix();
+})
+guiWallFolder.add(camera_wall, 'zoom', 0.01, 10, 0.01).name("Zoom").onChange(()=>{
+  camera_wall.updateProjectionMatrix();
+})
+
+// const controls_wall = new OrbitControls(camera_wall, canvas_wall)
+// controls_wall.enableDamping = true
+// controls_wall.enableZoom = true
+
+const renderer_wall = new THREE.WebGLRenderer({
+  canvas: canvas_wall,
+  antialias: true,
+  alpha: true,
+})
+renderer_wall.setSize(wall_sizes.width, wall_sizes.height)
+renderer_wall.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+let object_wall = null;
+
+objLoader.load(
+  "/objects/wall.obj",
+  ( object ) => {
+
+    object_wall = object;
+    object.scale.set( objectScales.wall,objectScales.wall,objectScales.wall );
+
+    object.traverse( function ( child ) {
+      if ( child instanceof THREE.Mesh ) {
+        switch( child.material.name ){
+          case 'Highlight':
+            child.material = material_vinylRed
+            break;
+          case 'Darker':
+            child.material = material_vinylBrown
+            break;
+          default:
+            child.material = material_vinylBeige
+            break;
+        }
+      }
+    });
+
+    scene_wall.add( object );
+  },
+);
 
 
 // Animations ————————————————————————————————————————————————————————
@@ -118,9 +220,16 @@ const tick = () =>
 
     // Controls
     controls_teapot.update()
+    // controls_wall.update()
+
+    // camera_wall.position.x = wall_cursor.x * wall_sizes.cameraMovement
+    camera_wall.position.x = Math.sin( wall_cursor.x * Math.PI / 10 ) * 100
+    camera_wall.position.z = Math.cos( wall_cursor.x * Math.PI / 10 ) * 100
+    camera_wall.position.y = wall_cursor.y * wall_sizes.cameraMovement + wall_sizes.cameraY
 
     // Renderers
     renderer_teapot.render(scene_teapot, camera_teapot)
+    renderer_wall.render(scene_wall, camera_wall)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
@@ -142,7 +251,13 @@ window.addEventListener('resize', () => {
   camera_teapot.aspect = teapot_sizes.width / teapot_sizes.height
   camera_teapot.updateProjectionMatrix()
 
+  camera_wall.aspect = wall_sizes.width / wall_sizes.height
+  camera_wall.updateProjectionMatrix()
+
   // Update renderer
   renderer_teapot.setSize(teapot_sizes.width, teapot_sizes.height)
   renderer_teapot.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  renderer_wall.setSize(wall_sizes.width, wall_sizes.height)
+  renderer_wall.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
